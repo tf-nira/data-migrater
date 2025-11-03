fc632ad0ab3b26545d424e8c25c26d40c3c761eb
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-11-03 10:49:43 +0530

    Updated upload job to for controlled upload

--- packet-generator/packet-uploader/src/main/java/io/mosip/packet/uploader/service/impl/PacketUploaderServiceImpl.java
+++ packet-generator/packet-uploader/src/main/java/io/mosip/packet/uploader/service/impl/PacketUploaderServiceImpl.java
@@ -44,6 +44,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -79,6 +80,9 @@ public class PacketUploaderServiceImpl  implements PacketUploaderService {
 
         @Value("${packet.manager.account.name}")
         private String packetUploadPath;
+        
+        @Value("${mosip.packet.archive.required:true}")
+        private boolean archivePacketZip;
 
         private RetryTemplate retryTemplate;
 
@@ -269,12 +273,19 @@ public class PacketUploaderServiceImpl  implements PacketUploaderService {
             String status = (String) responseBody.get(RegistrationConstants.UPLOAD_STATUS);
 
             if(status.equals("Packet has reached Packet Receiver")) {
-                Path path = Paths.get(System.getProperty("user.dir"), "home/Archieve");
-                File archieveFile = path.toFile();
-                if(!archieveFile.exists())
-                    archieveFile.mkdirs();
-
-                packet.renameTo(new File(path.toAbsolutePath().toString() + "//" + packet.getName()));
+                if (archivePacketZip) {
+					Path path = Paths.get(System.getProperty("user.dir"), "home/Archieve");
+					File archieveFile = path.toFile();
+					if (!archieveFile.exists())
+						archieveFile.mkdirs();
+					packet.renameTo(new File(path.toAbsolutePath().toString() + "//" + packet.getName()));
+				} else {
+					try {
+				        Files.deleteIfExists(packet.toPath());
+				    } catch (IOException e) {
+				    	LOGGER.info("Error while deleting file: " + e.getMessage());
+				    }
+				}
             }
 
             return status;
